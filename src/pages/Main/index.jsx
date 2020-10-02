34399d9addc4f680bcc58f8c6a8bd1980a6b3174
Gabriel Henrique Kuhn <67128059+gabrielkuhn@users.noreply.github.com>
2020-10-01 22:32:21 -0300

    Setup browser geolocation and weather API fetch

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import LoadingDots from '../../components/LoadingDots';
+import api from '../../services/api'
+import './styles.css'
+import '../../components/TemperatureCard/temperature-card.component'
+import { TemperatureCard, LoadingDots } from '../../components'
 
 export default function Main() {
-  return (
-    <LoadingDots/>
+  const [weather, setWeather] = useState()
+  const [cityCoords, setCityCoords] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  
+  useEffect(() => {
+    const fetchWeather = async (coords) => {
+      const response = await api.get('/onecall', {
+        params: {
+          lat: coords.latitude,
+          lon: coords.longitude,
+          appid: process.env.REACT_APP_API_KEY
+        }
+      })
+      setWeather(response.data)
+      setIsLoading(false)
+    }
+
+    const getGeolocation = () => {
+      console.log('Getting geolocation coords from the browser...')
+      navigator.geolocation.getCurrentPosition((pos) => {
+        const { latitude, longitude } = pos.coords
+        const geolocationCoords = { latitude, longitude }
+        setCityCoords(geolocationCoords)
+        console.log(`Coords returned with sucess.`)
+      })
+    }
+
+    if (!cityCoords) {
+      getGeolocation()
+    } else {
+      fetchWeather(cityCoords)
+    }
+
+  }, [cityCoords])
+
+  useEffect(() => {
+    if (weather) {
+      console.log('The current weather were sucessfull fetched')
+      console.log(weather)
+    }
+  }, [weather])
+  return ( 
+    <div className='section'>
+      {isLoading ? <LoadingDots /> : <TemperatureCard /> }
+    </div>
   )
 }
-
